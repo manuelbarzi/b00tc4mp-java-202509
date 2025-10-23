0219ddee56dca0722055c32404506aecdb485c37
manuelbarzi <manuelbarzi@gmail.com>
2025-10-23 18:41:02 +0200

    add users servlet to manage users; implement users registration; #3

--- staff/manuel-barzi/product/api/src/main/java/com/b00tc4mp/api/HelloServlet.java
+++ staff/manuel-barzi/product/api/src/main/java/com/b00tc4mp/api/HelloServlet.java
@@ -15,6 +15,7 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
+        
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>Hello, World!</h1>");
